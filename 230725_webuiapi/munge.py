518bb6defa3cb7d3e49646ec8468a144b586f616
Kyle Steinfeld <ksteinfe@gmail.com>
2023-07-25 20:58:30 -0700

    date

--- 230725_webuiapi/munge.py
+++ 230725_webuiapi/munge.py
@@ -1,20 +1,51 @@
 import webuiapi #https://github.com/mix1009/sdwebuiapi
 import requests
+from datetime import datetime
+from PIL import Image
+timestamp = datetime.now().strftime("%y%m%d_%H%M%S")
+
+api = webuiapi.WebUIApi(host='de6b0d3fe5f8a90f0c.gradio.live', port=443, use_https=True)
+print("api defined")
+
+#options = api.get_options()
+#print(options)
+
+
+img_depth = Image.open("img_dpth/02.jpg")
+print("depth image loaded")
+
+
+
+unit1 = webuiapi.ControlNetUnit(
+    input_image=img_depth, 
+    module='depth', 
+    model='control_v11f1p_sd15_depth [cfd03158]', 
+    weight=1.0
+)
+#print(unit1.to_dict())
+print("ControlNetUnit defined")
+
+rslt1 = api.txt2img(
+            prompt="a brutalist parking garage full of graffiti",
+            width=512,
+            height=512,
+            controlnet_units=[unit1],
+            sampler_name="Euler a",
+            cfg_scale=7,
+           )
+rslt1.image.save('{}.png'.format(timestamp))
+
+# upscale
+rslt2 = api.extra_single_image(image=rslt1.image,
+                                 upscaler_1=webuiapi.Upscaler.ESRGAN_4x,
+                                 upscaling_resize=2)
+print(rslt2.image.size)
+rslt2.image.save('{}_up.png'.format(timestamp))
 
 
 
 
-api = webuiapi.WebUIApi(host='06a9237bb7d0322410.gradio.live', use_https=True)
 
-print("api made")
-result1 = api.txt2img(prompt="cute squirrel",
-                    negative_prompt="ugly, out of frame",
-                    seed=1003,
-                    styles=["anime"],
-                    cfg_scale=7,
-                    )
-
-print(result1.info)
 
 
 """
@@ -26,6 +57,6 @@ params = {
     'attachment': 'false',
 }
 
-response = requests.get('https://06a9237bb7d0322410.gradio.live/internal/sysinfo', params=params, headers=headers)
-print(response)
+response = requests.get('https://de6b0d3fe5f8a90f0c.gradio.live/internal/sysinfo', params=params, headers=headers)
+print(response.content)
 """
---
+++ 230725_webuiapi/notes.md
@@ -0,0 +1,6 @@
+
+
+# 230725
+
+once gradio server is running, go to https://XXXXX.gradio.live/docs for API documentation
+
