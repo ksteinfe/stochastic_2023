f2bf62e462ad2457eebff80ab6bd11909dc0e51b
Kyle Steinfeld <ksteinfe@gmail.com>
2023-07-25 18:58:29 -0700

    adsf

---
+++ 230725_webuiapi/munge.py
@@ -0,0 +1,31 @@
+import webuiapi #https://github.com/mix1009/sdwebuiapi
+import requests
+
+
+
+
+api = webuiapi.WebUIApi(host='06a9237bb7d0322410.gradio.live', use_https=True)
+
+print("api made")
+result1 = api.txt2img(prompt="cute squirrel",
+                    negative_prompt="ugly, out of frame",
+                    seed=1003,
+                    styles=["anime"],
+                    cfg_scale=7,
+                    )
+
+print(result1.info)
+                    
+
+"""
+headers = {
+    'accept': 'application/json',
+}
+
+params = {
+    'attachment': 'false',
+}
+
+response = requests.get('https://06a9237bb7d0322410.gradio.live/internal/sysinfo', params=params, headers=headers)
+print(response)
+"""
